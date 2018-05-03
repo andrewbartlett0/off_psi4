18b3a55ebb2b156a545387194d3ecde142abd10c
Victoria Lim <lim.t.victoria@gmail.com>
2018-05-03 12:12:29 -0700

    add functionality to write all confs of one mol

--- off_psi4/tools/writeOneMol.py
+++ off_psi4/tools/writeOneMol.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 
-## By: Victoria T. Lim
+"""
+This script writes out either (1) a single molecule from a multi-molecule file
+or (2) a single conformer of some mol from file having many molecules
+with multiple conformers of each. This script can be used, for example, to
+isolate specific conformers that lead to a high RMSD energy value for a particular molecule.
 
-## This script writes out a single conformer of some mol from
-## an SDF file having many multi-conformer molecules.
-## This can be used, for example, to isolate specific conformers
-## that lead to a high RMSD energy value for a particular molecule.
+Usage: python writeOneMol.py -f inputfile.sdf -t molName -s sdtag -v tagvalue -x filesuffix
 
-## Usage: python writeOneConf.py -f inputfile.sdf -t molName -s sdtag -v tagvalue -x filesuffix
+By: Victoria T. Lim
+
+"""
 
 import os, sys
 import openeye.oechem as oechem
@@ -15,6 +18,25 @@ import argparse
 
 
 ### ------------------- Script -------------------
+
+def write_conf_mol(outfn, mol):
+    """
+
+
+    Parameters
+    ----------
+    outfn : str
+        Filename of the output file
+    mol : OpenEye oemol
+        Single or multi-conformer molecule to write to output
+
+    """
+
+    ofs = oechem.oemolostream()
+    if not ofs.open(outfn):
+        oechem.OEThrow.Fatal("Unable to open %s for writing" % outfn)
+    oechem.OEWriteConstMolecule(ofs, mol)
+    ofs.close()
 
 def main(**kwargs):
     outfn = os.path.splitext(opt['infn'])[0]+'_'+opt['suffix']+'.mol2'
@@ -29,19 +51,16 @@ def main(**kwargs):
 
     for mol in ifs.GetOEMols():
         if mol.GetTitle() == opt['title']:
+            # write out all confs in this mol if no SD tag is specified
+            if opt['sdtag'] == "":
+                success = True
+                write_conf_mol(outfn, mol)
+                return
+            # look for the conformer in this mol with specific SD tag value
             for i, conf in enumerate( mol.GetConfs()):
                 if oechem.OEGetSDData(conf, opt['sdtag']) == opt['value']:
-                    #print oechem.OEGetSDData(conf, opt['sdtag'])
-                    #print opt['value']
-                    ofs = oechem.oemolostream()
-                    #if os.path.exists(outfn):
-                    #    print "Output .sdf file already exists. Exiting.\n"
-                    #    return
-                    if not ofs.open(outfn):
-                        oechem.OEThrow.Fatal("Unable to open %s for writing" % outfn)
-                    oechem.OEWriteConstMolecule(ofs, conf)
-                    ofs.close()
                     success = True
+                    write_conf_mol(outfn, conf)
     if not success:
         print("\n** Found no confs matching your criteria. **")
     ifs.close()
@@ -49,12 +68,12 @@ def main(**kwargs):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("-f", "--infn",
+    parser.add_argument("-f", "--infn", required=True,
         help="Name of input SDF file from which to write out the conformer.")
-    parser.add_argument("-t", "--title",
+    parser.add_argument("-t", "--title", required=True,
         help="Molecule name in the file.")
-    parser.add_argument("-s", "--sdtag",
-        help="SD tag to search for conformer.")
+    parser.add_argument("-s", "--sdtag", default="",
+        help="SD tag to search for conformer. Leave blank if you want all conformers of this molecule.")
     parser.add_argument("-v", "--value",
         help="Value of the SD tag to write out that conformer.")
     parser.add_argument("-x", "--suffix",
@@ -65,7 +84,10 @@ if __name__ == "__main__":
 
     ### Check that input file exists.
     if not os.path.exists(opt['infn']):
-        raise parser.error("Input file %s does not exist. Try again." % opt['infn'])
+        raise parser.error("Error: Input file %s does not exist. Try again." % opt['infn'])
+    ### Check that value was specified if SD tag was specified
+    if opt['sdtag'] != "" and opt['value'] is None:
+        raise parser.error("Error: Specify a value for the SD tag to be searched.")
 
     main(**opt)
 
