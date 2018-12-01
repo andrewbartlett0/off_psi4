7e938fbdce558cf631e9b69f22fd7c9a2940edea
Victoria Lim <lim.t.victoria@gmail.com>
2018-11-30 18:16:36 -0800

    json to write input file, commit 1

--- quanformer/confs_to_psi.py
+++ quanformer/confs_to_psi.py
@@ -3,30 +3,45 @@
 """
 Purpose:    Generate Psi4 inputs for many molecules/conformers.
 By:         Victoria T. Lim, Christopher I. Bayly
-Version:    Oct 12 2018
+Version:    Nov 30 2018
 
 """
 
 import os, sys
 import openeye.oechem as oechem
 import shutil
+import json
 
 
 def make_psi_input(mol, label, method, basisset, calctype='opt', mem=None):
     """
+    Get coordinates from input mol, and generate/format input text for
+    Psi4 calculation.
+
     Parameters
     ----------
-    mol: single OEChem conformer with coordinates
-    label: string - name of the molecule. Can be an empty string.
-    method: string - specification of method (see Psi4 website for options)
-    basisset: string - specification of basis set
-    calctype: string - one of 'opt','spe','hess' for geometry optimization,
-        single point energy calculation, or Hessian calculation
-    mem: string - specify Psi4 job memory. E.g. "2 Gb" "2000 Mb" "2000000 Kb"
+    mol : OpenEye OEMol
+        OEMol with coordinates
+    label : string
+        Name of molecule with integer identifier (for conformers).
+    method: string
+        Name of the method as understood by Psi4. Example: "mp2"
+    basis : string
+        Name of the basis set as understood by Psi4. Example: "def2-sv(p)"
+    calctype : string
+        What kind of Psi4 calculation to run. Supported inputs are:
+        'opt' for geometry optimization,
+        'spe' for single point energy calculation, and
+        'hess' for Hessian calculation.
+    memory : string
+        How much memory each Psi4 job should take. If not specified, the
+        default in Psi4 is 500 Mb. Examples: "2000 MB" "1.5 GB"
+        http://www.psicode.org/psi4manual/master/psithoninput.html
 
     Returns
     -------
-    inputstring: string - containing contents of whole input file for this conf
+    inputstring : string
+        Contents of Psi4 input file to be written out
 
     """
 
@@ -35,7 +50,6 @@ def make_psi_input(mol, label, method, basisset, calctype='opt', mem=None):
         sys.exit("Specify a valid calculation type.")
 
     inputstring = ""
-    xyz = oechem.OEFloatArray(3)
 
     # specify memory requirements, if defined
     if mem != None:
@@ -46,7 +60,8 @@ def make_psi_input(mol, label, method, basisset, calctype='opt', mem=None):
     netCharge = oechem.OENetCharge( mol)
     inputstring+=( '  %s 1' % netCharge )
 
-    # get coordinates of each atom
+    # get atomic symbol and coordinates of each atom
+    xyz = oechem.OEFloatArray(3)
     for atom in mol.GetAtoms():
         mol.GetCoords( atom, xyz)
         inputstring+=( '\n  %s %10.4f %10.4f  %10.4f' \
@@ -93,17 +108,149 @@ def make_psi_input(mol, label, method, basisset, calctype='opt', mem=None):
     return inputstring
 
 
-def confs_to_psi(insdf, method, basis, calctype='opt', memory=None):
+def make_psi_json(mol, label, method, basisset, calctype='opt', mem=None):
     """
+    Get coordinates from input mol, and generate/format input text for
+    Psi4 calculation via JSON wrapper.
+
     Parameters
     ----------
-    insdf:  string - PATH+name of SDF file
-    method: string - method. E.g. "mp2"
-    basis:  string - basis set. E.g. "def2-sv(p)"
-    calctype: string - one of 'opt','spe','hess' for geometry optimization,
-                       single point energy calculation, or Hessian calculation
-    memory: string - memory specification. Psi4 default is 256 Mb. E.g. "1.5 Gb"
+    mol : OpenEye OEMol
+        OEMol with coordinates
+    label : string
+        Name of molecule with integer identifier (for conformers).
+    method: string
+        Name of the method as understood by Psi4. Example: "mp2"
+    basis : string
+        Name of the basis set as understood by Psi4. Example: "def2-sv(p)"
+    calctype : string
+        What kind of Psi4 calculation to run. Supported inputs are:
+        'opt' for geometry optimization,
+        'spe' for single point energy calculation, and
+        'hess' for Hessian calculation.
+    memory : string
+        How much memory each Psi4 job should take. If not specified, the
+        default in Psi4 is 500 Mb. Examples: "2000 MB" "1.5 GB"
+        http://www.psicode.org/psi4manual/master/psithoninput.html
 
+    Returns
+    -------
+    inputstring : string
+        Contents of Psi4 input file to be written out
+
+    """
+    # check that specified calctype is valid
+    if calctype not in {'opt','spe','hess'}:
+        sys.exit("Specify a valid calculation type.")
+
+    inputdict = {}
+    moldict = {}
+    modeldict = {}
+    keydict = {}
+    inputdict["schema_name"] = "qc_schema_input"
+    inputdict["schema_version"] = 1
+
+    # specify memory requirements, if defined
+    if mem != None:
+        inputdict["memory"] = mem
+
+    #TODO
+    # charge and multiplicity; multiplicity hardwired to singlet (usually is)
+    #inputdict["charge"] = oechem.OENetCharge( mol)
+    #inputdict["multiplicity"] = 1
+
+    # get atomic symbol and coordinates of each atom
+    geom_list = []
+    elem_list = []
+    xyz = oechem.OEFloatArray(3)
+    for atom in mol.GetAtoms():
+        mol.GetCoords( atom, xyz)
+        geom_list.append(xyz[0])
+        geom_list.append(xyz[1])
+        geom_list.append(xyz[2])
+        elem_list.append(oechem.OEGetAtomicSymbol(atom.GetAtomicNum()))
+    moldict["geometry"] = geom_list
+    moldict["symbols"] = elem_list
+    inputdict["molecule"] = moldict
+
+    #TODO
+    ## check if mol has a "freeze" tag
+    #for x in oechem.OEGetSDDataPairs(mol):
+    #    if calctype=="opt" and "atoms to freeze" in x.GetTag():
+    #        b = x.GetValue()
+    #        y = b.replace("[", "")
+    #        z = y.replace("]", "")
+    #        a = z.replace(" ", "")
+    #        freeze_list = a.split(",")
+    #        inputstring += ("\n\nfreeze_list = \"\"\"\n  {} xyz\n  {} xyz\n  {} "
+    #                       "xyz\n  {} xyz\n\"\"\"".format(freeze_list[0],
+    #                       freeze_list[1], freeze_list[2], freeze_list[3]))
+    #        inputstring += "\nset optking frozen_cartesian $freeze_list"
+    #        inputstring += ("\nset optking dynamic_level = 1\nset optking "
+    #            "consecutive_backsteps = 2\nset optking intrafrag_step_limit = "
+    #            "0.1\nset optking interfrag_step_limit = 0.1\n")
+
+    #TODO
+    ## explicitly specify MP2 RI-auxiliary basis for Ahlrichs basis set
+    ## http://www.psicode.org/psi4manual/master/basissets_byfamily.html
+    ## DFMP2 *should* get MP2 aux sets fine for Pople/Dunning
+    ## http://www.psicode.org/psi4manual/master/dfmp2.html
+    #if method.lower()=='mp2' and 'def2' in basisset:
+    #    if basisset.lower()=='def2-sv(p)':
+    #        inputstring+=('\nset df_basis_mp2 def2-sv_p_-ri')
+    #    elif basisset.lower()!='def2-qzvpd':  # no aux set for qzvpd 10-6-18
+    #        inputstring+=('\nset df_basis_mp2 %s-ri' % (basisset))
+
+    modeldict["basis"] = basisset
+    modeldict["method"] = method
+    inputdict["model"] = modeldict
+
+    #inputstring+=('\nset freeze_core True')
+    # specify command for type of calculation
+    if calctype=='opt':
+        # TODO
+        pass
+    elif calctype=='spe':
+        inputdict["driver"] = 'energy'
+    elif calctype=='hess':
+        inputdict["driver"] = 'hessian'
+        keydict["return_wfn"] = True
+    inputdict["keywords"] = keydict
+
+    return inputdict
+
+
+def confs_to_psi(insdf, method, basis, calctype='opt', memory=None, via_json=False):
+    """
+    Read in molecule(s) (and conformers, if present) in insdf file. Create
+    Psi4 input calculations for each structure.
+
+    Parameters
+    ----------
+    insdf: string
+        Name of the molecule file for which to create Psi4 input file.
+        SDF format can contain multiple molecules and multiple conformers per
+        molecule in a single file.
+    method: string
+        Name of the method as understood by Psi4. Example: "mp2"
+    basis : string
+        Name of the basis set as understood by Psi4. Example: "def2-sv(p)"
+    calctype : string
+        What kind of Psi4 calculation to run. Supported inputs are:
+        'opt' for geometry optimization,
+        'spe' for single point energy calculation, and
+        'hess' for Hessian calculation.
+    memory : string
+        How much memory each Psi4 job should take. If not specified, the
+        default in Psi4 is 500 Mb. Examples: "2000 MB" "1.5 GB"
+        http://www.psicode.org/psi4manual/master/psithoninput.html
+    via_json : Boolean
+        If True, use JSON wrapper for Psi4 input and output.
+        - Psi4 input would be in "input.py", called with python
+        - Psi4 output would be in "output.json"
+        If False, use normal text files for Psi4 input and output.
+        - Psi4 input would be in "input.dat"
+        - Psi4 output would be in "output.dat"
     """
     wdir = os.getcwd()
 
@@ -128,7 +275,17 @@ def confs_to_psi(insdf, method, basis, calctype='opt', memory=None):
                 print("Input file (\"input.dat\") already exists. Skipping.\n")
                 continue
             label = mol.GetTitle()+'_'+str(i+1)
-            ofile = open(os.path.join(subdir,'input.dat'), 'w')
-            ofile.write(make_psi_input( conf, label, method, basis, calctype, memory))
+            if via_json:
+                ofile = open(os.path.join(subdir,'input.py'), 'w')
+                ofile.write("# molecule {}\n\nimport numpy as np\nimport psi4"
+                            "\nimport json\n\njson_data = ".format(label))
+                json.dump(make_psi_json( conf, label, method, basis, calctype, memory), ofile, indent=4, separators=(',', ': '))
+                ofile.write("\njson_ret = psi4.json_wrapper.run_json(json_data)\n\n")
+                ofile.write("with open(\"output.json\", \"w\") as ofile:\n\t"
+                            "json.dump(json_ret, ofile, indent=2)\n\n")
+            else:
+                ofile = open(os.path.join(subdir,'input.dat'), 'w')
+                ofile.write(make_psi_input( conf, label, method, basis, calctype, memory))
             ofile.close()
     ifs.close()
+
--- tests/test_confs_to_psi.py
+++ tests/test_confs_to_psi.py
@@ -51,12 +51,18 @@ def test_make_dfmp2_svpp():
 def test_confs_to_psi():
     confs_to_psi(os.path.join(mydir,'data_tests','methane_c2p.sdf'),'mp2','def2-sv(p)')
     # check file byte size (this line should be updated if confs_to_psi changes)
-    #assert os.path.getsize(os.path.join(mydir,'methane','1','input.dat')) == 327
     assert os.path.getsize(os.path.join('methane','1','input.dat')) == 327
+    shutil.rmtree('methane')
+    return
+
+def test_confs_to_psi_json():
+    confs_to_psi(os.path.join(mydir,'data_tests','methane_c2p.sdf'),
+                'mp2','def2-sv(p)',calctype='spe',via_json=True)
+    # check file byte size (this line should be updated if confs_to_psi changes)
+    assert os.path.getsize(os.path.join('methane','1','input.py')) == 1032
     #shutil.rmtree('methane')
     return
 
-
 # test manually without pytest
 if 0:
-    test_confs_to_psi()
+    test_confs_to_psi_json()
